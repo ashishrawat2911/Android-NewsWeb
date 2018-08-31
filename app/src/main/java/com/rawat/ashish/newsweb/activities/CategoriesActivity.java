ba34e2e81e7bb8cad6e5d675502486e17b776690
Ashish Rawat <ashishrawat2911@gmail.com>
2018-08-31 19:01:17 +0530

    added menu functionality

--- app/src/main/java/com/rawat/ashish/newsweb/activities/CategoriesActivity.java
+++ app/src/main/java/com/rawat/ashish/newsweb/activities/CategoriesActivity.java
@@ -1,10 +1,15 @@
 package com.rawat.ashish.newsweb.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.rawat.ashish.newsweb.R;
 import com.rawat.ashish.newsweb.adaptor.CategoriesAdaptor;
@@ -17,6 +22,7 @@ public class CategoriesActivity extends AppCompatActivity {
     List<Categories> categoriesList;
     RecyclerView categoriesRecyclerView;
     CategoriesAdaptor categoriesAdaptor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +49,39 @@ public class CategoriesActivity extends AppCompatActivity {
         categoriesList.add(new Categories(R.drawable.science_news, "Science", "science"));
         categoriesList.add(new Categories(R.drawable.politics_news, "Politics", "politics"));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menuRate:
+                String packageName = getApplicationContext().getPackageName();
+                String playStoreLink = "https://play.google.com/store/apps/details?id=" + packageName;
+                Intent appRateUsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(playStoreLink));
+                startActivity(appRateUsIntent);
+                return true;
+            case R.id.menuShare:
+                String packageName2 = getApplicationContext().getPackageName();
+                Intent appShareIntent = new Intent(Intent.ACTION_SEND);
+                appShareIntent.setType("text/plain");
+                String extraText = "Hey! Check out this amazing app all News Categories.\n\n";
+                extraText += "https://play.google.com/store/apps/details?id=" + packageName2;
+                appShareIntent.putExtra(Intent.EXTRA_TEXT, extraText);
+                startActivity(appShareIntent);
+                return true;
+            case R.id.menuAbout:
+                Toast.makeText(getApplicationContext(), "Item 3 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
